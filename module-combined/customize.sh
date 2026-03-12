9a0987c945ccaafbfc9a9db5a5248538b9626833
thunderkex <thunderkex@gmail.com>
2026-03-12 07:48:04 +0700

    handle install errors by clearing residual package data
    Signed-off-by: thunderkex <thunderkex@gmail.com>

--- module-combined/customize.sh
+++ module-combined/customize.sh
@@ -135,7 +135,11 @@ install_app() {
 				if echo "$op" | grep -q -e INSTALL_FAILED_VERSION_DOWNGRADE -e INSTALL_FAILED_UPDATE_INCOMPATIBLE; then
 					ui_print "* Handling install error"
 					pmex uninstall-system-updates "$PKG_NAME"
-					BASEPATH=$(pmex path "$PKG_NAME") || abort
+					if ! BASEPATH=$(pmex path "$PKG_NAME"); then
+						ui_print "* Clearing residual package data..."
+						pmex uninstall "$PKG_NAME" >/dev/null 2>&1 || true
+						continue
+					fi
 					BASEPATH=${BASEPATH##*:} BASEPATH=${BASEPATH%/*}
 					if [ "${BASEPATH:1:4}" != data ]; then IS_SYS=true; fi
 					if [ "$IS_SYS" = true ]; then
