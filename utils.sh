520bcccd7e1cbad56a5783428aedccce3db2ab88
j-hc <j-hc@users.noreply.github.com>
2026-03-21 20:46:46 +0300

    update merge_splits
    Signed-off-by: thunderkex <thunderkex@gmail.com>

--- utils.sh
+++ utils.sh
@@ -529,14 +529,11 @@ merge_splits() {
 		epr "APKEditor error: $OP"
 		return 1
 	fi
-	# sign the merged stock apk
-	if ! OP=$(java -jar "$APKSIGNER" sign --ks ks-p12.keystore --ks-pass pass:123456789 --key-pass pass:123456789 --ks-key-alias jhc \
-		--out "${output}" "${output}-unsigned"); then
-		epr "apksigner error: $OP"
-		return 1
-	fi
-	rm "${output}.idsig" "${output}-unsigned" 2>/dev/null || :
-	return 0
+	# sign the merged apk properly
+	patch_apk "${bundle}.mzip" "${output}" "--exclusive" "${args[cli]}" "${args[ptjar]}"
+	local ret=$?
+	rm -f "${bundle}.mzip"
+	return $ret
 }
 
 # -------------------- apkmirror --------------------
