9f3562466b2212988886bd0dc17b121192d9970f
thunderkex <thunderkex@gmail.com>
2026-04-01 16:26:21 +0700

    fix: handle empty patch versions in get_patch_last_supported_ver
    Signed-off-by: thunderkex <thunderkex@gmail.com>

--- utils.sh
+++ utils.sh
@@ -430,6 +430,7 @@ get_patch_last_supported_ver() {
 		fi
 	fi
 	op=$(patches_list_versions "$cli_jar" "$patches_jar" "$pkg_name") || return 1
+	if grep -qE "^\s*Any\s*$" <<<"$op"; then return; fi
 	op=$(sed -n '/(.* patch.*/,$p' <<<"$op" | awk '{$1=$1}1')
 	if [ "$op" = "Any" ]; then return; fi
 	pcount=$(head -1 <<<"$op") pcount=${pcount#*(} pcount=${pcount% *}
